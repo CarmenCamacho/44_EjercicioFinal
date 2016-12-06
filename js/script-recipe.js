7d77e91f6250560fd0beb2ef7977d2063fb46a8a
CarmenCamacho <carmen.camacho.rodriguez@gmail.com>
2016-12-06 11:43:09 -0600

    Etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,6 +1,18 @@
 $(document).ready( function(){
 	console.log("Estamos listos!")
-	
-	$(".js-menu").hide();	
+
+	$(".js-menu").hide();
+
+	 $(".js-show-recipe").click(function(){
+ 		$(".page").removeClass("make");
+	 });
+ 	
+	 $(".js-show-make").click(function(){
+ 		$(".page").addClass("make");
+ 	});	
 
 });
+	
+
+
+
